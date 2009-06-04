f96c3fd491254e584cba9558fa4d39d08b154b0e
Giacomo Ritucci <giacomo.ritucci@email.it>
2009-06-05 00:15:07 +0200

    classes for audio and udp ports

---
+++ ports.lisp
@@ -0,0 +1,55 @@
+;; ULB-SIM
+;; UDP Load Balancing SIMulator
+
+;; Copyright (C) 2009  Giacomo Ritucci
+
+;; Redistribution and use in source and binary forms, with or without
+;; modification, are permitted provided that the following conditions
+;; are met:
+;;
+;;   1. Redistributions of source code must retain the above copyright
+;;      notice, this list of conditions and the following disclaimer.
+;;   2. Redistributions in binary form must reproduce the above
+;;      copyright notice, this list of conditions and the following
+;;      disclaimer in the documentation and/or other materials
+;;      provided with the distribution.
+;;
+;; THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED
+;; WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+;; OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+;; DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+;; INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+;; (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+;; SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+;; HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+;; STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+;; OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+(declaim (optimize debug safety (speed 0)))
+
+
+;; OVERVIEW
+
+;; TODO
+
+
+(in-package :ulb-sim)
+
+
+
+(defclass audio-in-port (in-port)
+  nil)
+
+
+(defclass audio-out-port (out-port)
+  nil)
+
+
+(defclass udp-in-port (in-port)
+  nil)
+
+
+(defclass udp-out-port (out-port)
+  nil)
