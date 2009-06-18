7f913d01c39187c4e7ed0cae665e66ed03c70441
Giacomo Ritucci <giacomo.ritucci@email.it>
2009-06-18 19:57:02 +0200

    unit conversions

--- objects.lisp
+++ objects.lisp
@@ -43,3 +43,82 @@
     :initform (error ":duration missing")
     :reader duration-of
     :type fixnum)))
+
+
+(defun usecs (us)
+  us)
+
+(defun msecs (ms)
+  (* ms (usecs 1000)))
+
+(defun secs (s)
+  (* s (msecs 1000)))
+
+(defun mins (m)
+  (* m (secs 60)))
+
+
+(defun bits (b)
+  b)
+
+(defun bytes (b)
+  (* b (bits 8)))
+
+
+(defun kilobits (kb)
+  (* kb (bits (expt 10 3))))
+
+(defun kilobytes (kb)
+  (* kb (bytes (expt 10 3))))
+
+(defun kibibits (kib)
+  (* kib (bits (expt 2 10))))
+
+(defun kibibytes (kib)
+  (* kib (bytes (expt 2 10))))
+
+
+(defun megabits (mb)
+  (* mb (bits (expt 10 6))))
+
+(defun megabytes (mb)
+  (* mb (bytes (expt 10 6))))
+
+(defun mebibits (mib)
+  (* mib (bits (expt 2 20))))
+
+(defun mebibytes (mib)
+  (* mib (bytes (expt 2 20))))
+
+
+(defun bits-per-second (bps)
+  (/ (bits bps) (secs 1)))
+
+(defun bytes-per-second (bps)
+  (/ (bytes bps) (secs 1)))
+
+
+(defun kilobits-per-second (kbps)
+  (/ (kilobits kbps) (secs 1)))
+
+(defun kilobytes-per-second (kbps)
+  (/ (kilobytes kbps) (secs 1)))
+
+(defun kibibits-per-second (kibps)
+  (/ (kibibits kibps) (secs 1)))
+
+(defun kibibytes-per-second (kibps)
+  (/ (kibibytes kibps) (secs 1)))
+
+
+(defun megabits-per-second (mbps)
+  (/ (megabits mbps) (secs 1)))
+
+(defun megabytes-per-second (mbps)
+  (/ (megabytes mbps) (secs 1)))
+
+(defun mebibits-per-second (mbps)
+  (/ (mebibits mbps) (secs 1)))
+
+(defun mebibytes-per-second (mbps)
+  (/ (mebibytes mbps) (secs 1)))
