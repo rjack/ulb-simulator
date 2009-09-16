0460b59b9d9cadfda850d7fbf0fda78f43bf1ed4
Giacomo Ritucci <giacomo.ritucci@email.it>
2009-09-16 23:06:36 +0200

    mini scenario di prova sp-sp

--- ulb-snr.lisp
+++ ulb-snr.lisp
@@ -31,64 +31,10 @@
 ;(declaim (optimize (debug 0) (safety 0) speed))
 
 
-(defpackage :it.unibo.cs.web.ritucci.ulb-snr
-  (:nicknames :ulb-snm)
-  (:use :common-lisp :de-sim :ulb-sim)
-  (:export))
-(in-package :ulb-snr)
+(defparameter *a-sp* (new 'sphone-sim))
+(defparameter *b-sp* (new 'sphone-sim))
 
-
-;; ULB SCENARIO SIMULATOR
-;; Istanzia e connette vari simulatori per costruire una situazione.
-
-
-;; definizione dello scenario
-;; E' un simulatore che ha come componenti i simulatori che compongono
-;; lo scenario.
-(defclass ulb-scenario (sim)
-  ((sp-alice :initarg :sp-alice :type sphone-sim)
-   (ulb      :initarg :ulb      :type ulb-sim)
-   (wlan0    :initarg :wlan0    :type wlan-sim)
-   (wlan1    :initarg :wlan1    :type wlan-sim)
-   (ap0      :initarg :ap0      :type apoint-sim)
-   (ap1      :initarg :ap1      :type apoint-sim)
-   (proxy    :initarg :proxy    :type proxy-sim)
-   (sp-bob   :initarg :sp-bob   :type sphone-sim)))
-
-
-;; prova di creazione scenario
-(let* ((qos (pick-one 'none 'simple 'probabilistic))
-       (alice-talk (pick-one 'wild-talk 'balanced 'monosillabic))
-       (bob-talk (pick-one 'wild-talk 'balanced 'monosillabic))
-       ;; ogni tipo di link dovrebbe avere valori di default
-       ;; appropriati.
-       (links (list (udp<-> 'sp-alice 'ulb)
-		    (wifi<-> 'ulb 'ap0
-			     :bw (mib/s 18)
-			     :error-rate (% 10)
-			     :delay (usecs 1))
-		    (wifi<-> 'ulb 'ap1
-			     :bw (mib/s 13)
-			     :error-rate (% 3)
-			     :delay (usecs 1))
-		    (udp<=> 'ap0 'proxy
-			    :bw (mib/s 10)
-			    :error-rate (% 2)
-			    :delay (msecs 30))
-		    (udp<=> 'ap1 'proxy
-			    :bw (mib/s 12)
-			    :error-rate (% 3)
-			    :delay (msecs 17))
-		    (udp<=> 'proxy 'sp-bob
-			    :bw (mib/s 1)
-			    :error-rate (% 1)
-			    :delay (msecs 50))))
-       (snr (new 'ulb-scenario
-		 :sp-alice (new 'sphone-sim :talk alice-talk)
-		 :ulb (new 'ulb-sim :qos qos)
-		 :ap0 (new 'apoint-sim)
-		 :ap1 (new 'apoint-sim)
-		 :proxy (new 'proxy-sim :qos qos)
-		 :sp-bob (new 'sphone-sim :talk (bob-talk))
-		 :lm (new 'link-manager :links links))))
-  nil)
+(connect! (out *a-sp*)
+	  (in *b-sp*))
+(connect! (out *b-sp*)
+	  (in *a-sp*))
